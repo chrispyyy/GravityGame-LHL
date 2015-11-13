12b3775a4712cdcc56620c6cbfdf5ea80c8e42c2
Rahim Jadavji <rahim.jadavji@gmail.com>
2015-11-13 15:32:13 -0800

    basic collision system, changes color of ship when it hits stuff

--- game_object.js
+++ game_object.js
@@ -2,7 +2,10 @@ function GameObject (name, size, mass, scene, x, y, z){
 
   this.canvasObject = BABYLON.Mesh.CreateSphere(name, 16, size, scene);
 
+  this.material = this.canvasObject.material = new BABYLON.StandardMaterial(name, scene);
+
   this.mass = mass; 
+
   this.size = size;
 
   this.position = this.canvasObject.position = new BABYLON.Vector3(x, y, z);
