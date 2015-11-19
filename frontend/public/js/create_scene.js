ce91b6db560058b8ef17a72ba2366ed9cc272f1f
Nick Jordan <cnicholasjordan@gmail.com>
2015-11-18 22:08:13 -0800

    refactored extra for loop

--- frontend/public/js/create_scene.js
+++ frontend/public/js/create_scene.js
@@ -25,9 +25,8 @@ module.exports = function createScene(engine, canvas){
 
   var canvasObjects = [];
 
-  for (var i=0; i<1; i++) {
-    canvasObjects[i] = new GameObject('planet', 12, 30, scene, 25, 1, 25);
-  }
+
+  canvasObjects[0] = new GameObject('planet', 12, 30, scene, 25, 1, 25);
 
   canvasObjects[0] = plutoTexture(scene, canvasObjects[0])
 
