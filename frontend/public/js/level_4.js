cf9a8d3c0b79bff93f28c4ccc862a0e8d86a6c8f
Nick Jordan <cnicholasjordan@gmail.com>
2015-11-24 16:35:53 -0800

    level 4 created

--- frontend/public/js/level_4.js
+++ frontend/public/js/level_4.js
@@ -3,20 +3,28 @@ var planetTexture = require('./planet_texture.js');
 var asteroidTexture = require('./asteroid_texture.js');
 
 module.exports.ship = function(scene){
-  var spaceship = new Ship('ship', 2, .5, scene, -20, 1, -20);
+  var spaceship = new Ship('ship', 2, .5, scene, -75, 1, -30);
   return spaceship;
 }
 
 module.exports.canvasObjects = function(scene){
   var canvasObjects = [];
 
-  canvasObjects[0] = new GameObject('planet', 12, 30, scene, 25, 1, 25);
+  canvasObjects[0] = new GameObject('planet', 12, 50, scene, 75, 1, 30);
   
   canvasObjects[0] = planetTexture(scene, canvasObjects[0], "./public/images/venusmap.jpg", "./public/images/venusbump.png");
 
-  canvasObjects[1] = new GameObject('asteroid', 4, 5, scene, 10, 1, -6);
-  canvasObjects[2] = new GameObject('asteroid', 4, 5, scene, -6, 1, 10)
-  canvasObjects[3] = new GameObject('asteroid', 4, 5, scene, 4, 1, 4);
+  canvasObjects[1] = new GameObject('asteroid', 4, 2, scene, -30, 1, 36);
+  canvasObjects[2] = new GameObject('asteroid', 4, 2, scene, -30, 1, 12);
+  canvasObjects[3] = new GameObject('asteroid', 4, 2, scene, -30, 1, 24);
+  canvasObjects[4] = new GameObject('asteroid', 4, 2, scene, 0, 1, -36);
+  canvasObjects[5] = new GameObject('asteroid', 4, 2, scene, 0, 1, -24);
+  canvasObjects[6] = new GameObject('asteroid', 4, 2, scene, 30, 1, 36)
+  canvasObjects[7] = new GameObject('asteroid', 4, 2, scene, 30, 1, 24)
+  canvasObjects[8] = new GameObject('asteroid', 4, 2, scene, 30, 1, 12);
+  canvasObjects[9] = new GameObject('asteroid', 4, 2, scene, -30, 1, 48);
+  canvasObjects[10] = new GameObject('asteroid', 4, 2, scene, 0, 1, -48);
+  canvasObjects[11] = new GameObject('asteroid', 4, 2, scene, 30, 1, 48);
 
   for (var i = 1; i < canvasObjects.length; i++) {
     canvasObjects[i] = asteroidTexture(scene, canvasObjects[i]);
