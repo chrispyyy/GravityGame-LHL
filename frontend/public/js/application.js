1ac0b1401f2c8b3d7dd102e22f7e962de3ce7be6
Rahim Jadavji <rahim.jadavji@gmail.com>
2015-11-25 15:09:19 -0800

    menu added

--- frontend/public/js/application.js
+++ frontend/public/js/application.js
@@ -48,9 +48,82 @@ window.addEventListener('DOMContentLoaded', function()
     }
   });
 
-  $('<button>Start Game</button>').appendTo('#next-level')
-  $('#next-level img').attr('src', scenes[currentLevel].image)
+  $('.container').on('click', '.main-menu', function() {
+    var main = "<img class='title' src='./public/images/blackhole_title.png'>" +
+                "<div class='box'>" +
+                "<div class='new-game'>" +
+                  "<img src='./public/images/new-game.png'>" +
+                "</div>" +
+                "<div class='load-level'>" +
+                  "<img src='./public/images/load-level.png'>" +
+                "</div>" +
+                "<div class='end-game'>" +
+                  "<img src='./public/images/end-game.png'>" +
+                "</div>";
+    $('.menu-box').html(main);
+  });
 
+  $('.container').on('click', '.load-level', function() {
+    var levels = "<img class='main-menu' src='./public/images/main-menu.png'>" +
+                 "<div class='level one'>" +
+                    "<img src='./public/images/level-1.png'>" +
+                  "</div>" +
+                  "<div class='level two'>" +
+                    "<img src='./public/images/level-2.png'>" +
+                  "</div>" +
+                  "<div class='level three'>" +
+                    "<img src='./public/images/level-3.png'>" +
+                  "</div>" +
+                  "<div class='level four'>" +
+                    "<img src='./public/images/level-4.png'>" +
+                  "</div>" + 
+                  "<div class='level five'>" +
+                    "<img src='./public/images/level-5.png'>" +
+                  "</div>" +
+                  "<div class='level six'>" +
+                    "<img src='./public/images/level-6.png'>" +
+                  "</div>" +
+                  "<div class='level seven'>" +
+                    "<img src='./public/images/level-7.png'>" +
+                  "</div>" + 
+                  "<div class='level eight'>" +
+                    "<img src='./public/images/level-8.png'>" +
+                  "</div>" +
+                  "<div class='level nine'>" +
+                    "<img src='./public/images/level-9.png'>" +
+                  "</div>";
+    $('.menu-box').html(levels);
+  }); 
+    
+  $('.container').on('click', '.end-game', function(){
+    scene = null
+  }); 
+
+  function addContainer(cssClass, level){
+    $('.container').on('click', cssClass, function(){
+      currentLevel = level;
+      $('.container').hide();
+      $('#next-level button').text('Start Game');
+      $('#next-level img').attr('src', scenes[currentLevel].image);
+      $('#next-level').fadeIn();
+      $('#menu-button').fadeIn();
+    });
+  }
+
+  $('#menu-button').on('click', function(){
+    $('.container').slideDown();
+    $('#menu-button').hide();
+  });
+
+
+  addContainer('.new-game', 0);
+
+  var cssClassArr = ['.one', '.two', '.three', '.four', '.five', '.six', '.seven', '.eight', '.nine'];
+
+  for(var i=0; i < scenes.length; i++){
+    addContainer(cssClassArr[i], i);
+  }
+ 
   $('#next-level').on('click', 'button', function() {
     scene = createScene(engine, canvas, scenes[currentLevel].scene);
     $('#next-level').fadeOut();
