ce93e21859b32577f75220a5a593cf4fd9ab6143
Rahim Jadavji <rahim.jadavji@gmail.com>
2015-11-24 16:03:53 -0800

    have some game instructions

--- frontend/public/js/application.js
+++ frontend/public/js/application.js
@@ -5,12 +5,6 @@ var createScene = require('./create_scene.js');
 var level1 = require('./level_1.js')
 var level2 = require('./level_2.js')
 var level3 = require('./level_3.js')
-var level4 = require('./level_4.js')
-var level5 = require('./level_5.js')
-var level6 = require('./level_6.js')
-var level7 = require('./level_7.js')
-var level8 = require('./level_8.js')
-var level9 = require('./level_9.js')
 var PubSub = require('pubsub-js');
 var $ = require("jquery");
 
@@ -25,13 +19,7 @@ window.addEventListener('DOMContentLoaded', function()
   var scenes = [
     {scene: level1, image: level1.image}, 
     {scene: level2, image: level2.image},
-    {scene: level3, image: level3.image},  
-    {scene: level4, image: level4.image},  
-    {scene: level5, image: level5.image},  
-    {scene: level6, image: level6.image},  
-    {scene: level7, image: level7.image},  
-    {scene: level8, image: level8.image},  
-    {scene: level9, image: level9.image},  
+    {scene: level3, image: level3.image},
   ];
 
   
@@ -53,8 +41,28 @@ window.addEventListener('DOMContentLoaded', function()
 
   $('#next-level').on('click', 'button', function() {
     scene = createScene(engine, canvas, scenes[currentLevel].scene);
-
     $('#next-level').fadeOut();
+    if(currentLevel == 0){
+      var instruction1 = $('<div>');
+      instruction1.addClass('game-instruction');
+      instruction1.text('Click anywhere on the screen to generate Black Holes. The longer you press down, the larger they get.');
+
+      var instruction2 = $('<div>');
+      instruction2.addClass('game-instruction');
+      instruction2.text('The objective of the Game is to navigate to the Planet, watch out for obstacles on the way!!');
+
+      instruction1.appendTo('body');
+      instruction2.appendTo('body');
+
+      setTimeout(function(){
+        instruction1.fadeIn('slow').delay(4000);
+        instruction1.fadeOut();
+      }, 500)
+      setTimeout(function(){
+        instruction2.fadeIn('slow').delay(4000);
+        instruction2.fadeOut();
+      }, 6000)
+    }
   });
 
   var collisionSubscriber = function(msg, data){
